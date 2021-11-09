3215e192bb5b14807ed0888ede609c3d85360e9f
Xvirus9 <58216944+Xvirus9-dev@users.noreply.github.com>
2021-11-09 11:10:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import pygame
+import os
+import win32api
+import win32file
+
+devices = []
+
+drives = win32api.GetLogicalDriveStrings().split('\x00')[:-1]
+
+for device in drives:
+    devices.append(device)
+    
+print(devices)
+ 
+def scanDevices():
+    verif = False
+    newDevices = []
+    drives = win32api.GetLogicalDriveStrings().split('\x00')[:-1]
+    
+    for device in drives:
+        newDevices.append(device)
+
+    if devices == newDevices:
+        verif = True        
+    return verif
+
+
+
+
+pygame.init()
+pygame.font.init()
+
+clock = pygame.time.Clock()
+number = 0
+width, height = (1920,1080)
+
+screen = pygame.display.set_mode((width, height))
+icon = pygame.image.load('icon.jpg')
+pygame.display.set_icon(icon)
+pygame.display.set_caption("VéloEnergie")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (30, 30, 30)
+FONT = pygame.font.SysFont("Comic Sans MS", 50)
+
+button = pygame.Rect(0, 100, 200, 200)
+textsurface = FONT.render("Vous pouvez brancher votre vélo...", False, (0, 0, 0))
+
+
+def blitRotate(surf, image, pos, originPos, angle):
+    # offset from pivot to center
+    image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1] - originPos[1]))
+    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
+
+    # roatated offset from pivot to center
+    rotated_offset = offset_center_to_pivot.rotate(-angle)
+
+    # roatetd image center
+    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
+
+    # get a rotated image
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_image_rect = rotated_image.get_rect(center=rotated_image_center)
+
+    # rotate and blit the image
+    surf.blit(rotated_image, rotated_image_rect)
+
+    # draw rectangle around the image
+    #pygame.draw.rect(surf, (255, 0, 0), (*rotated_image_rect.topleft, *rotated_image.get_size()), 2)
+loading = pygame.transform.scale(pygame.image.load('loading.gif'), (100, 100))
+w, h = loading.get_size()
+angle = 0
+
+
+
+while True:
+    pygame.time.Clock().tick(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
+    color = (59, 226, 203)
+    
+    if not scanDevices():
+        color = (124, 169, 113)
+        sprite_image = pygame.image.load("bar.gif").convert_alpha()
+        
+        
+        
+        
+        
+        pygame.display.flip()  
+    else: 
+        screen.blit(textsurface, (width/2-400,height/2+50))
+        pos = (screen.get_width() / 2, screen.get_height() / 2)
+        blitRotate(screen, loading, pos, (w / 2, h / 2), angle)
+        angle += 1
+
+        pygame.display.flip()
+
+    
+    screen.fill(color)
+
+
+    
+    
+
+
+pygame.quit()
